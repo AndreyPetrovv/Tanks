e2fd302c11a60240a3378e89721b383124dba6cc
Andrreyj <42888775+Andrreyj@users.noreply.github.com>
2018-10-10 21:18:56 +0700

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tanks
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Random r = new Random();
+            Tank tankOne = new Tank("1", r.Next(50, 760), r.Next(50, 390));
+            canvas.Children.Add(tankOne.drawingTankBo());
+            canvas.Children.Add(tankOne.drawingTankBa());
+            Canvas.SetLeft(tankOne.drawingTankBo(), tankOne.PositionToX);
+            Canvas.SetTop(tankOne.drawingTankBo(), tankOne.PositionToY);
+            Canvas.SetLeft(tankOne.drawingTankBa(), tankOne.PositionToX);
+            Canvas.SetTop(tankOne.drawingTankBa(), tankOne.PositionToY);
+        }
+    }
+}
---
+++ Tank.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Tanks 
+{
+    public class Tank 
+    {
+
+        private string _name;
+        private int _positionToX;
+        private int _positionToY;
+        public int PositionToX
+        {
+            get
+            {
+                return _positionToX;
+            }
+            set
+            {
+                _positionToX = value;
+            }
+        }
+        public int PositionToY
+        {
+            get
+            {
+                return _positionToY;
+            }
+            set
+            {
+                _positionToY = value;
+            }
+        }
+
+        private string Name {
+            get
+            {
+                return _name;
+            }
+            set {
+                _name = value;
+            }
+        }
+
+        BodyTank tankBo ;
+        Tank_barrel tankBa ;
+
+        public Tank(string name, int x, int y) {
+            PositionToX = x;
+            PositionToY = y;
+            _name = name;
+            tankBo = new BodyTank(x,y);
+            tankBa = new Tank_barrel(x, y);
+           
+        }
+        public Tank() { }
+
+        public Rectangle drawingTankBo() {
+            
+            return tankBo.GetBody();
+        }
+        public Line drawingTankBa() {
+            
+            return tankBa.GetBarrel();
+        }
+
+        public virtual void drawing() { }
+
+        public virtual void Move() { }
+
+        public virtual void Shot() { }
+
+    }
+}
